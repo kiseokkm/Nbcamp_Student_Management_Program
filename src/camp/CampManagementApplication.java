bca2ef01f63936d189e261cf72b10c6fc229312c
inho9979 <inho9979@naver.com>
2024-05-03 17:42:20 +0900

    :sparkles: update: ì 수강생정보 등록 기능 구현

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -163,19 +163,22 @@ public class CampManagementApplication {
 
     // 수강생 등록
     private static void createStudent() {
-        System.out.println("\n수강생을 등록합니다...");
-        System.out.print("수강생 이름 입력: ");
-        String studentName = sc.next();
-        // 기능 구현 (필수 과목, 선택 과목)
+//        System.out.println("\n수강생을 등록합니다...");
+//        System.out.print("수강생 이름 입력: ");
+//        String studentName = sc.next();
 
-        Student student = new Student(sequence(INDEX_TYPE_STUDENT), studentName); // 수강생 인스턴스 생성 예시 코드
-        // 기능 구현
+        // 기능 구현 (필수 과목, 선택 과목)
+        StudentManage.createStudent(sc);
+
+//      Student student = new Student(sequence(INDEX_TYPE_STUDENT), studentName); // 수강생 인스턴스 생성 예시 코드
+
         System.out.println("수강생 등록 성공!\n");
     }
 
     // 수강생 목록 조회
     private static void inquireStudent() {
         System.out.println("\n수강생 목록을 조회합니다...");
+        StudentManage.inquiryStudent();
         // 기능 구현
         System.out.println("\n수강생 목록 조회 성공!");
     }
---
+++ src/camp/StudentManage.java
@@ -0,0 +1,40 @@
+package camp;
+
+import camp.model.Student;
+import camp.model.Subject;
+
+import java.util.*;
+
+public class StudentManage
+{
+    public  static Map<String, Student> stu_list = new HashMap<>();
+    private static int studentIndex;
+    private static final String INDEX_TYPE_STUDENT = "ST";
+
+    // 이곳에서 수강생 등록 수행
+    public static void createStudent(Scanner sc) {
+        System.out.println("\n수강생을 등록합니다...");
+        System.out.print("수강생 이름 입력: ");
+        String studentName = sc.next();
+        // 기능 구현 (필수 과목, 선택 과목)
+
+        Student student = new Student(INDEX_TYPE_STUDENT + studentIndex++, studentName,
+                SubjectManage.subjectSelect(sc));
+
+        stu_list.put(student.getStudentId(),student);
+    }
+
+    // 임시 테스트용
+    public static void inquiryStudent(){
+        for(Student e : stu_list.values()) {
+            System.out.println("ID: " + e.getStudentId() + " 이름: " + e.getStudentName());
+            System.out.println("수강중인 과목들:");
+            for(Subject f : e.getSubjectList())
+            {
+                System.out.printf("과목명: " + f.getSubjectName() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
