c8aaa1cf5870af940526bfc15f8260fed2e98980
inho9979 <inho9979@naver.com>
2024-05-05 15:15:13 +0900

    update :sparkles: ㅇã 다중입력, í, 필수선택표시 기능, 코드정리

--- src/camp/SubjectList.java
+++ src/camp/SubjectList.java
@@ -3,19 +3,21 @@ package camp;
 import camp.model.Subject;
 
 public enum SubjectList {
-    JAVA("Java"),
-    OOP("객체지향"),
-    SPRING("Spring"),
-    JPA("JPA"),
-    MYSQL("MySQL"),
-    DESIGN_PETTERN("디자인패턴"),
-    SPRING_SECURITY("Spring_Security"),
-    REDIS("Redis"),
-    MONGODB("MongoDB");
+    JAVA("Java","MANDATORY"),
+    OOP("객체지향","MANDATORY"),
+    SPRING("Spring","MANDATORY"),
+    JPA("JPA","MANDATORY"),
+    MYSQL("MySQL","MANDATORY"),
+    DESIGN_PETTERN("디자인패턴","CHOICE"),
+    SPRING_SECURITY("Spring_Security","CHOICE"),
+    REDIS("Redis","CHOICE"),
+    MONGODB("MongoDB","CHOICE");
 
     public final String name;
+    public final String type;
 
-    SubjectList(String subName) {
+    SubjectList(String subName, String subType) {
         name = subName;
+        type = subType;
     }
 }
